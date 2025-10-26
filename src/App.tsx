4685a43d1fae86942a87876670e83f54b0743f7d
Mert Önalan <mert.onalan1@gmail.com>
2025-10-26 21:52:34 +0100

    feat(app): set up main App component with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.scss'
+import { Suspense, lazy } from "react";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingFallback from "./components/LoadingFallback/LoadingFallback";
+import { ROUTES } from "./routes";
 
-function App() {
-  return (
-    <div className='app'>
-      App
-    </div>
-  )
-}
+const MainPage = lazy(() => import("./pages/MainPage/MainPage"));
+const DescriptionPage = lazy(() => import("./pages/DescriptionPage/DescriptionPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
-export default App
+const App = () => {
+    return (
+        <Router>
+            <Toaster />
+            <Suspense fallback={<LoadingFallback />}>
+                <Routes>
+                    <Route path={ROUTES.MAIN} element={<MainPage />} />
+                    <Route path={ROUTES.COMPONENT_DETAILS} element={<DescriptionPage />} />
+                    <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
+                </Routes>
+            </Suspense>
+        </Router>
+    );
+};
+
+export default App;
